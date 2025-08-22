2b61b418b0aba64b5322e570d0ffb34b7cdfcef1
Kubbany <2021170289@cis.asu.edu.eg>
2025-08-22 19:37:20 +0300

    user service created

---
+++ src/modules/users/services/user.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import User from '../entites/user.entity';
+import { UserDTO } from '../DTO/user.dto';
+import * as bcrypt from 'bcryptjs';
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async createUser(userDto: UserDTO): Promise<User> {
+    const { email, name, password } = userDto;
+    const userExist = await this.userRepository.findOne({ where: { email } });
+    if (userExist) {
+      throw new ConflictException('Email Already Exists');
+    }
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const newUser = this.userRepository.create({
+      name,
+      email,
+      password: encryptedPassword,
+    });
+    return this.userRepository.save(newUser);
+  }
+}
