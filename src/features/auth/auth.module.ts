c31f461777dd9466d1a82a3ed03b202dde884320
Kubbany <2021170289@cis.asu.edu.eg>
2025-08-23 19:32:26 +0300

    login endpoint created

--- src/features/auth/auth.module.ts
+++ src/features/auth/auth.module.ts
@@ -3,9 +3,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import User from '../users/entites/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: config.get<string | number>('JWT_EXPIRES') },
+      }),
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
   exports: [AuthService],
--- src/features/auth/controllers/auth.controller.ts
+++ src/features/auth/controllers/auth.controller.ts
@@ -2,15 +2,23 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { RegisterRequestDTO } from '../dtos/register_request.dto';
 import { RegisterResponseDTO } from '../dtos/register_response.dto';
+import { LoginRequestDto } from '../dtos/login_request.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly userService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('register')
   async register(
     @Body() registerRequestDTO: RegisterRequestDTO,
   ): Promise<RegisterResponseDTO> {
-    return this.userService.register(registerRequestDTO);
+    return this.authService.register(registerRequestDTO);
+  }
+
+  @Post('login')
+  async login(
+    @Body() loginRequestDTO: LoginRequestDto,
+  ): Promise<{ token: string }> {
+    return this.authService.login(loginRequestDTO);
   }
 }
