96a05accddc5a4ddec07377bab5187ab33257169
Kubbany <2021170289@cis.asu.edu.eg>
2025-08-24 15:16:39 +0300

    create posts entity

---
+++ src/features/posts/entites/post.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('posts')
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({ nullable: true })
+  imageUrl: string;
+
+  @Column({ default: 0 })
+  votesCount: number;
+
+  @Column()
+  authorId: number;
+
+  @Column()
+  authorName: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
